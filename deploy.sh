c7ff67312de60788cb466d3144ac23f914efcb40
BrentWood <KiddMillion@gmai.com>
2024-05-25 00:58:36 -0400

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+DEVELOPERS AT WORK
 Application is ready to be deployed to the development evironment.
-deployment must be reviewed by managers before ASAP deployment.I
+deployment must be reviewed by managers before ASAP deployment
