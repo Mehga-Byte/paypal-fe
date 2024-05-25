7355fa9c40516d8d2b66addb5210018f66d69243
KingKillaByte <brentrdms@gmail.com>
2024-05-25 19:15:42 -0400

    completed

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ Application is ready to be deployed to the development evironment.
 deployment must be reviewed by managers before ASAP deployment HEAD
 AWS CodeDeploy is a great tool.
 SDLC is great
+performance
