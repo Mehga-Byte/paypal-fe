a7447f33622db90d056a4bc54dd5849b7130578e
Mehga-Byte <brentrdms@gmail.com>
2024-05-24 20:54:14 -0700

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 Application is ready to be deployed to the development evironment.
-deployment must be reviewed by managers before deployment.
+deployment must be reviewed by managers before ASAP deployment.I
